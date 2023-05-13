c1bafb78766c8951475eec02b8e80afca53065da
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-05-13 02:55:58 +0000

    Done webpage

--- index.php
+++ index.php
@@ -72,19 +72,19 @@
         echo "Please enter two integers.";
     } else {
     // use a for loop to calculate the product from the two integers
-    for ($counter = 0; $counter < $absSecondInt; $counter++) {
+        for ($counter = 0; $counter < $absSecondInt; $counter++) {
             $product += $absFirstInt;
-    }
+        }
 
     // if the firstInt is less than 0 and the secondInt is greater than 0, then make the product negative
-    if (($firstInt < 0) && ($secondInt > 0)) {
+        if (($firstInt < 0) && ($secondInt > 0)) {
             $product = $product * -1;
-    } elseif (($secondInt < 0) && ($firstInt > 0)) {
+        } elseif (($secondInt < 0) && ($firstInt > 0)) {
     // otherwise, if firstInt is greater than 0 and secondInt is less than 0, then make the product negative
             $product = $product * -1;
-    }
+        }
     // redefine calculation statement
-    $numbers = $firstInt . "*" . $secondInt . "=" . $product;
+        $numbers = $firstInt . "*" . $secondInt . "=" . $product;
     }
 
     // display the product
